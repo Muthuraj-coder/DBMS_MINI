f1091e645d098b76387b2047a19ed8d2346ace6a
Muthuraj-coder <muthurajias3232@gmail.com>
2025-05-08 19:07:58 +0530

    Commit

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -1,4 +1,3 @@
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -14,7 +13,7 @@ SECRET_KEY = '(i#*06f#keydy_fh17bf=$0f6v)^wr^l7*u4gq42m*sztu#2_m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
