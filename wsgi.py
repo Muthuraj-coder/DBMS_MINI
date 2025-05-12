ca9ec55bd6486df975e6a1d4ba34dc37de71e7f3
Muthuraj-coder <155149096+Muthuraj-coder@users.noreply.github.com>
2025-05-12 11:45:16 +0530

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+# DBMS_MINI/wsgi.py
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DBMS_MINI.settings')
+
+application = get_wsgi_application()
